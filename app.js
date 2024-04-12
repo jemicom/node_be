e07f09c1b1a793b5f7f578df0899684d7a9c3ee5
jemicom <jemicom@gmail.com>
2024-04-13 02:32:33 +0900

    root html 수정

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
 
 app.use(cors()); // 어떤 url 요청도 모두 들어주기, url을 제한할 필요가 있음 
 app.use(logger('dev'));
@@ -29,7 +29,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, "views" , 'index.html'))
+})
 app.use('/users', usersRouter);
 app.use('/board', boardRouter);
 app.use('/makeup', makeupRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
+var fs = require('fs')
 /* GET home page. */
 // localhost:4000
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'React Backend' });
-  res.send(fs.readFile('./views/index.html'))
+  // res.send(fs.readFile('./views/index.html'))
+  // res.sendFile( fs.readFile(path.join( '../views/index.html' )))
+  res.sendFile(path.join(__dirname + '../views/index.html'))
 });
 
 module.exports = router;
