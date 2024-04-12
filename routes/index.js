a3f56699ab9144b4b008649c877b0edc84195c6f
jemicom <jemicom@gmail.com>
2024-04-13 02:17:38 +0900

    widthout jade

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 // localhost:4000
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'React Backend' });
-  // res.send(fs.readFile('./views/index.html'))
+  // res.render('index', { title: 'React Backend' });
+  res.send(fs.readFile('./views/index.html'))
 });
 
 module.exports = router;
